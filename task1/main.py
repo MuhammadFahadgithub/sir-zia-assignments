0de4ce8923e79f682d5a55ae32bcfcd173b60b92
MUHAMMAD FAHAD <159857192+MuhammadFahadgithub@users.noreply.github.com>
2025-05-26 14:28:37 +0500

    Add files via upload

---
+++ task1/main.py
@@ -0,0 +1,38 @@
+teacher_name:str = str("Sir Asharib")
+#str is class of string
+
+class ABC(str):
+    pass
+
+teacher_name:str = ABC("Sir Asharib")
+
+# custom data type
+
+#just in time compilor read kerna
+
+#pyhton operators
+# string casting
+#control flow
+#python is scripting
+#if False:
+#print("PASS")
+#print("NOT PASS")
+
+lst:list=[1,2]
+
+tpl:tuple=(1,2)
+
+
+#pythone high level language
+teacher_name="Sir Asharib"
+mon_class_teacher = teacher_name
+teacher_name="Sir Ameen Alam"
+print(teacher_name)
+print(mon_class_teacher)
+
+
+def add():
+ return 1+1
+
+print(add())
+# function ki state ko terminate kerna - return ka kam hai
